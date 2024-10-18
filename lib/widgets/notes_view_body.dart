9449b2fea5fe7337475f9a96dfda3f7e0637c92f
AmmarHabbab <ammarhabbab118@gmail.com>
2024-10-18 21:50:10 +0300

    created notes list view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 
 import 'custom_note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -16,7 +17,12 @@ class NotesViewBody extends StatelessWidget {
             height: 25,
           ),
           CustomAppBar(),
-          NoteItem(),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+              child:
+                  NotesListView()), // to use expanded u must use with widgets that are flex like column and row but u cant use it in stack for example cuz its not flex and be aware that sometimes this may not throw and exception on screen
         ],
       ),
     );
