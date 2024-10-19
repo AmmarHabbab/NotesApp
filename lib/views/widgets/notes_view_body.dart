ef52b43ee69302eca17e3d00be5b2715faaaa5e4
AmmarHabbab <ammarhabbab118@gmail.com>
2024-10-19 04:17:33 +0300

    create note model

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
 import 'notes_list_view.dart';
 
